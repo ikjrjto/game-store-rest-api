35ae92fe3ef744b356e286b537dae3e412f9545c
Ivan Batiuk <ikjrjto@gmail.com>
2024-01-27 15:52:08 +0200

    Initial commit

---
+++ src/main/java/com/sumdu/edu/ua/ivan/gamestore/GameStoreRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sumdu.edu.ua.ivan.gamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumdu/edu/ua/ivan/gamestore/GameStoreRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumdu.edu.ua.ivan.gamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
